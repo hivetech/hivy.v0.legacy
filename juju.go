f25bbfcb4a1bbd5049c5fb79f7c945ad96e37d6e
hackliff <xavier.bruhiere@gmail.com>
2013-11-18 21:16:01 +0100

    tmp(): backup

--- juju.go
+++ juju.go
@@ -174,10 +174,10 @@ func (jj *Juju) Deploy(user, service string) (*simplejson.Json, error) {
   }
 
   // Charm deployment
-  //log.Infof("enqueue process")
-  //client, err := goresque.Dial(redisURL)
-  //if err != nil { return EmptyJSON(), err }
-  //client.Enqueue(workerClass, "fork", jj.Path, args)
+  log.Infof("enqueue process")
+  client, err := goresque.Dial(redisURL)
+  if err != nil { return EmptyJSON(), err }
+  client.Enqueue(workerClass, "fork", jj.Path, args)
 
   report.Set("deployed", id)
   report.Set("provider", "juju")
